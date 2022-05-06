01c0bf6e0a7fb49850588724914071e98029a83e
Thien <bananakind123@gmail.com>
2022-05-06 08:14:23 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Database, objectVal, ref, update } from '@angular/fire/database';
+import { traceUntilFirst } from '@angular/fire/performance';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'y';
+  flag: boolean = false;
+  data: any = {
+    limit: 0,
+    temperature: 0,
+  };
+  newValue: number = 0;
+  // myModal = <HTMLElement>document.getElementById('myModal')
+  // myInput = <HTMLElement>document.getElementById('myInput')
+
+
+  // public readonly testObjectValue$: Observable<any>;
+  constructor(private database: Database) {
+    const doc = ref(database, '/');
+    objectVal(doc).pipe(
+      traceUntilFirst('database')
+    ).subscribe((data: any) => {
+      this.data = data;
+      console.log(data);
+      if (data.limit <= data.temperature) {
+        this.flag = true;
+      }
+    });
+  }
+  abc() {
+    const doc = ref(this.database, '/');
+
+    update(doc, { limit: this.newValue });
+  }
+  ngOnInit(): void {
+  }
 }
